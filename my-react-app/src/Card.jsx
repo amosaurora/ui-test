ef28b09aed9e54359a095e5cf803b4bb724874ca
Amos <amosaurora@gmail.com>
2024-08-19 23:45:19 +0800

    Adds buttons

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+  return(
+    <div className="card">
+        <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
+        <h2 className="title">Amos</h2>
+        <p className="card-text">This is a test to learn about React</p>
+    </div>
+  );
+}
+
+export default Card
